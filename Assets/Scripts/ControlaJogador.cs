1090f703bdf58950e3e0fbc08b7a6f66b83b086e
Marcelo Ferreira <mah.tf@hotmail.com>
2020-04-14 16:21:46 -0300

    Adicionado a fisica das personagens - Dia 2

---
+++ Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+    public int Velocidade = 10;
+    Vector3 direcao;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixoX, 0, eixoZ);
+
+        //transform.Translate(direcao * Velocidade *  Time.deltaTime); //Esse codigo não é mais utilizado, alterado pelo FixedUpdate()
+
+        if(direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * Velocidade * Time.deltaTime));
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Apocalipse Zumbi 
+Este jogo está em desenvolvimento através de um curso ministrado pela Alura.
+
+## Objetivo do jogo
+Você é um dos últimos sobreviventes em um mundo pós apocaliptico apocalíptico, você precisa aprender a sobreviver neste mundo.
+
+
+#### Diário
+- Dia 1: Neste dia foi criado o cenário e personagem utilizando as artes disponibilizadas pela Alura, também foi adicionado a movimentação da personagem
+- Dia 2: Foi adicionado animações a personagem e as colisões utilizando física
+
+
+
+*Marcelo T. Ferreira*
